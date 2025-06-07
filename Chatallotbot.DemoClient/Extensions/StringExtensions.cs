aae104b16d2f5ab57667cbbd40a5ff86cd0ccadd
Bjørn Heber <bjorn.heber@advania.no>
2025-06-07 09:12:23 +0200

    Remove template

---
+++ Chatallotbot.DemoClient/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+using Markdig;
+namespace Chatallotbot.Web.Extensions;
+
+public static class StringExtensions
+{
+    // Copyright (c) David Pine. All rights reserved.
+    private static readonly MarkdownPipeline s_pipeline =
+        new MarkdownPipelineBuilder()
+            .ConfigureNewLine("\n")
+            .UseAdvancedExtensions()
+            .UseEmojiAndSmiley()
+            .UseSoftlineBreakAsHardlineBreak()
+            .Build();
+
+    public static string ToHtml(this string markdown) =>
+        string.IsNullOrWhiteSpace(markdown) is false
+            ? Markdown.ToHtml(markdown, s_pipeline)
+            : "";
+}
